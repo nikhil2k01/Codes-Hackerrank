9515b8dc6a3852ec1a0244b0ad047113980fcb1e
Nikhil Shah <nikhilshah3593@gmail.com>
2024-08-01 11:05:11 +0530

    Added Solutions of day 1

---
+++ Day1_Let And Const.js
@@ -0,0 +1,13 @@
+
+function main() {
+let r = 
+const PI = Math.PI;
+function area(PI, r) {
+  return  PI * r * r
+}
+
+function perimeter(PI, r) {
+  return 2 * PI * r
+}   
+console.log(area(PI, r));
+console.log(perimeter(PI, r));
